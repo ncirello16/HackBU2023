209f5eae7c3a68bfc756b2a97b80750c60b43d6e
ncirello16 <ncirello16@gmail.com>
2023-02-04 11:30:03 -0500

    We won

---
+++ We are winning.py
@@ -0,0 +1,11 @@
+favProfessor = input("What is your favorite professor? ")
+
+while favProfessor.upper() != "GEORGE":
+    print("Wrong answer.......................")
+    favProfessor = input("What is your favorite professor? ")
+
+print("Same, George is my fav professor too :)")
+
+
+
+
