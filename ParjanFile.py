181a692b86635577140db3fc0b8613ee4da858bd
Parjan ameen <parjan.ameen@gmail.com>
2023-02-04 16:33:04 -0500

    Moving cat sound to sound folder

--- ParjanFile.py
+++ ParjanFile.py
@@ -1,14 +1,3 @@
-# Simport vlc
 
-# def play_mp4_audio():
-#     filename = "Dog Barking Sound Effects _ No Copyright Sound Effects Free To Use.mp4"
-#     instance = vlc.Instance()
-#     player = instance.media_player_new()
-#     media = instance.media_new(filename)
-#     player.set_media(media)
-#     player.play()
-#
-#
-# play_mp4_audio()
 
 
