72d30a70b81e914d20c4772703f06f0f472df282
sirunde <sirunde@github.com>
2023-02-04 23:27:39 -0500

    updated DICBOT

--- find.py
+++ find.py
@@ -1,17 +1,46 @@
+import urllib3
 from bs4 import BeautifulSoup
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
+from selenium import webdriver
+from sys import platform
+
 
 def Diction(html):
-    URL = html
+    URL = f"https://www.merriam-webster.com/dictionary/{html}"
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
-    mydivs = soup.find("div",{"class": "sense-content w-100"})
-    my = mydivs.find("span", {"dtText"})
-    print(my.text[1:])
-    return my.text[1:]
+    try:
+        mydivs = soup.find("div",{"class": "sense-content w-100"})
+        my = mydivs.find("span", {"dtText"})
+        output = f"DicBot: {my.text[1:]}"
+
+    except:
+        output = f"DicBot: Cannot find {html}!"
+
+    return output
+
+def valentine(q):
+    if platform == "linux" or platform == "linux2":
+    # linux
+        pass
+    elif platform == "darwin":
+        PATH = "bin/chromedriver"
+    # OS X
+        pass
+    elif platform == "win32":
+
+        PATH = "bin/chromedriver.exe"
+
+    driver = webdriver.Chrome(PATH)
+    driver.get("https://amazon.com")
 
 
+    print(driver.title)
 if __name__ == "__main__":
 
     x = input("what do you want to know?\n")
-    Diction(f"https://www.merriam-webster.com/dictionary/{x}")
+    # Diction(f"https://www.merriam-webster.com/dictionary/{x}")
+    valentine(x)
